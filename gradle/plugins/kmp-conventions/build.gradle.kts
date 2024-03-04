14fd686f324e751b95d9978b924723a1861ee90c
s.zemlyakov <redtom@yandex.ru>
2024-03-04 18:19:19 +0300

    fix: build on system with missing Java21

--- gradle/plugins/kmp-conventions/build.gradle.kts
+++ gradle/plugins/kmp-conventions/build.gradle.kts
@@ -12,10 +12,3 @@ dependencies {
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
     implementation(libs.kotlin.gradle.plugin)
 }
-
-kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(libs.versions.java.compiler.version.get().toInt()))
-        vendor.set(JvmVendorSpec.matching(libs.versions.java.vendor.get()))
-    }
-}
