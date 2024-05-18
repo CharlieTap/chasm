31bce759aa5309a512dd946a7bec140f634bf3d1
CharlieTap <charlie.tapping@deliveroo.co.uk>
2024-05-18 22:19:07 +0100

    remove legacy table growth allocator

--- executor/instantiator/src/commonMain/kotlin/io/github/charlietap/chasm/executor/instantiator/allocation/table/TableGrowthAllocator.kt
+++
@@ -1,8 +0,0 @@
-package io.github.charlietap.chasm.executor.instantiator.allocation.table
-
-import com.github.michaelbull.result.Result
-import io.github.charlietap.chasm.executor.runtime.error.InvocationError
-import io.github.charlietap.chasm.executor.runtime.instance.TableInstance
-import io.github.charlietap.chasm.executor.runtime.value.ReferenceValue
-
-internal typealias TableGrowthAllocator = (TableInstance, UInt, ReferenceValue) -> Result<TableInstance, InvocationError.TableGrowExceedsLimits>
