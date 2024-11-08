fe78117b886b83fce834f1ab2afe69a1de4b1233
CharlieTap <charlie.tapping@deliveroo.co.uk>
2024-11-08 18:50:01 +0000

    relax build vendor requirement

--- gradle/plugins/kmp-conventions/src/main/kotlin/kmp-conventions.gradle.kts
+++ gradle/plugins/kmp-conventions/src/main/kotlin/kmp-conventions.gradle.kts
@@ -28,6 +28,5 @@ kotlin {
 
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.java.compiler.version.get().toInt()))
-        vendor.set(JvmVendorSpec.matching(libs.versions.java.vendor.get()))
     }
 }
--- gradle/plugins/publishing-conventions/build.gradle.kts
+++ gradle/plugins/publishing-conventions/build.gradle.kts
@@ -17,7 +17,6 @@ dependencies {
 kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.java.compiler.version.get().toInt()))
-        vendor.set(JvmVendorSpec.matching(libs.versions.java.vendor.get()))
     }
 }
 
