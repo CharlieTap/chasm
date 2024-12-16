6e671b1ee64cb6ed82e55651e4de59f48162528b
CharlieTap <charlie.tapping@deliveroo.co.uk>
2024-12-16 18:56:35 +0000

    leverage stack shrinking to avoid temporary allocations

--- executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/control/ReturnExecutor.kt
+++ executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/control/ReturnExecutor.kt
@@ -2,40 +2,20 @@ package io.github.charlietap.chasm.executor.invoker.instruction.control
 
 import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.binding
-import io.github.charlietap.chasm.executor.invoker.ext.forEachReversed
-import io.github.charlietap.chasm.executor.runtime.Stack.Entry.InstructionTag
 import io.github.charlietap.chasm.executor.runtime.error.InvocationError
 import io.github.charlietap.chasm.executor.runtime.execution.ExecutionContext
 import io.github.charlietap.chasm.executor.runtime.ext.popFrame
-import io.github.charlietap.chasm.executor.runtime.ext.popInstruction
-import io.github.charlietap.chasm.executor.runtime.ext.popLabel
-import io.github.charlietap.chasm.executor.runtime.ext.popValue
 import io.github.charlietap.chasm.executor.runtime.instruction.ControlInstruction
 
 internal inline fun ReturnExecutor(
     context: ExecutionContext,
     instruction: ControlInstruction.Return,
 ): Result<Unit, InvocationError> = binding {
+
     val (stack) = context
     val frame = stack.popFrame().bind()
 
-    val results = List(frame.arity.value) {
-        stack.popValue().bind()
-    }
-
-    do {
-        val instruction = stack.popInstruction().bind()
-    } while (instruction.tag != InstructionTag.FRAME)
-
-    while (stack.labelsDepth() > frame.stackLabelsDepth) {
-        stack.popLabel().bind()
-    }
-
-    while (stack.valuesDepth() > frame.stackValuesDepth) {
-        stack.popValue().bind()
-    }
-
-    results.forEachReversed { value ->
-        stack.push(value)
-    }
+    stack.shrinkLabels(0, frame.stackLabelsDepth)
+    stack.shrinkInstructions(0, frame.stackInstructionsDepth)
+    stack.shrinkValues(frame.arity.value, frame.stackValuesDepth)
 }
