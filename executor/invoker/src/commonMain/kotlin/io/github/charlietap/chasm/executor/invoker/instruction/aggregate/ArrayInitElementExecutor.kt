307fe799dc458130fada1e3793abcbe67a39bf32
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-01-27 19:25:00 +0000

    prevent defined type expansion on array init element

--- executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/aggregate/ArrayInitElementExecutor.kt
+++ executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/aggregate/ArrayInitElementExecutor.kt
@@ -1,12 +1,10 @@
 package io.github.charlietap.chasm.executor.invoker.instruction.aggregate
 
-import io.github.charlietap.chasm.executor.invoker.ext.definedType
 import io.github.charlietap.chasm.executor.invoker.ext.elementAddress
 import io.github.charlietap.chasm.executor.runtime.error.InvocationError
 import io.github.charlietap.chasm.executor.runtime.exception.InvocationException
 import io.github.charlietap.chasm.executor.runtime.execution.ExecutionContext
 import io.github.charlietap.chasm.executor.runtime.ext.array
-import io.github.charlietap.chasm.executor.runtime.ext.arrayType
 import io.github.charlietap.chasm.executor.runtime.ext.element
 import io.github.charlietap.chasm.executor.runtime.ext.popArrayReference
 import io.github.charlietap.chasm.executor.runtime.instruction.AggregateInstruction
@@ -32,11 +30,8 @@ internal inline fun ArrayInitElementExecutor(
     val stack = context.vstack
     val store = context.store
 
-    val (typeIndex, elementIndex) = instruction
+    val elementIndex = instruction.elementIndex
     val frame = context.cstack.peekFrame()
-    val definedType = frame.instance.definedType(typeIndex)
-
-    val arrayType = definedTypeExpander(definedType).arrayType()
 
     val elementAddress = frame.instance
         .elementAddress(elementIndex)
@@ -60,7 +55,7 @@ internal inline fun ArrayInitElementExecutor(
         val elementIndex = sourceOffsetInElementSegment + offset
         val elementValue = elementInstance.elements[elementIndex]
         val fieldIndex = destinationOffsetInArray + offset
-        val fieldValue = fieldPacker(elementValue, arrayType.fieldType)
+        val fieldValue = fieldPacker(elementValue, arrayInstance.arrayType.fieldType)
 
         arrayInstance.fields[fieldIndex] = fieldValue
     }
