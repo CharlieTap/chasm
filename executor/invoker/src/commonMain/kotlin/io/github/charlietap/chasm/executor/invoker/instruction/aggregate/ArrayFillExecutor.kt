389912a1c5f8165e4dd67b507f444347b3d31383
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-01-27 19:21:49 +0000

    prevent defined type expansion on array fill

--- executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/aggregate/ArrayFillExecutor.kt
+++ executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/aggregate/ArrayFillExecutor.kt
@@ -1,14 +1,11 @@
 package io.github.charlietap.chasm.executor.invoker.instruction.aggregate
 
-import io.github.charlietap.chasm.executor.invoker.ext.definedType
 import io.github.charlietap.chasm.executor.runtime.error.InvocationError
 import io.github.charlietap.chasm.executor.runtime.exception.InvocationException
 import io.github.charlietap.chasm.executor.runtime.execution.ExecutionContext
 import io.github.charlietap.chasm.executor.runtime.ext.array
-import io.github.charlietap.chasm.executor.runtime.ext.arrayType
 import io.github.charlietap.chasm.executor.runtime.ext.popArrayReference
 import io.github.charlietap.chasm.executor.runtime.instruction.AggregateInstruction
-import io.github.charlietap.chasm.type.expansion.DefinedTypeExpander
 
 internal fun ArrayFillExecutor(
     context: ExecutionContext,
@@ -16,18 +13,17 @@ internal fun ArrayFillExecutor(
 ) = ArrayFillExecutor(
     context = context,
     instruction = instruction,
-    definedTypeExpander = ::DefinedTypeExpander,
     fieldPacker = ::FieldPacker,
 )
 
 internal inline fun ArrayFillExecutor(
     context: ExecutionContext,
     instruction: AggregateInstruction.ArrayFill,
-    crossinline definedTypeExpander: DefinedTypeExpander,
     crossinline fieldPacker: FieldPacker,
 ) {
     val store = context.store
     val stack = context.vstack
+
     val elementsToFill = stack.popI32()
     val fillValue = stack.pop()
     val arrayElementOffset = stack.popI32()
@@ -40,14 +36,10 @@ internal inline fun ArrayFillExecutor(
 
     if (elementsToFill == 0) return
 
-    val frame = context.cstack.peekFrame()
-    val definedType = frame.instance.definedType(instruction.typeIndex)
-    val arrayType = definedTypeExpander(definedType).arrayType()
-
     repeat(elementsToFill) { fillOffset ->
 
         val fieldIndex = arrayElementOffset + fillOffset
-        val fieldValue = fieldPacker(fillValue, arrayType.fieldType)
+        val fieldValue = fieldPacker(fillValue, arrayInstance.arrayType.fieldType)
 
         arrayInstance.fields[fieldIndex] = fieldValue
     }
