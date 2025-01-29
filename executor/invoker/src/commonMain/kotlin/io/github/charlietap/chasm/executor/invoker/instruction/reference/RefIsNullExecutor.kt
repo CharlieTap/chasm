b4803e4bfcbe19d0ec0964b7052bb776511d9403
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-01-29 20:27:22 +0000

    remove boxed transformation in refisnull

--- executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/reference/RefIsNullExecutor.kt
+++ executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/reference/RefIsNullExecutor.kt
@@ -1,18 +1,17 @@
 package io.github.charlietap.chasm.executor.invoker.instruction.reference
 
 import io.github.charlietap.chasm.executor.runtime.execution.ExecutionContext
-import io.github.charlietap.chasm.executor.runtime.ext.popReference
+import io.github.charlietap.chasm.executor.runtime.ext.isNullableReference
 import io.github.charlietap.chasm.executor.runtime.instruction.ReferenceInstruction
-import io.github.charlietap.chasm.executor.runtime.value.ReferenceValue
 
 internal inline fun RefIsNullExecutor(
     context: ExecutionContext,
     instruction: ReferenceInstruction.RefIsNull,
 ) {
     val stack = context.vstack
-    val value = stack.popReference()
+    val value = stack.pop()
 
-    if (value is ReferenceValue.Null) {
+    if (value.isNullableReference()) {
         stack.push(1L)
     } else {
         stack.push(0L)
