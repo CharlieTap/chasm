8030b2164153073fc02af53d162907c5f491870c
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-01-29 18:54:31 +0000

    prevent boxing on ref as non null

--- executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/reference/RefAsNonNullExecutor.kt
+++ executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/reference/RefAsNonNullExecutor.kt
@@ -1,20 +1,19 @@
 package io.github.charlietap.chasm.executor.invoker.instruction.reference
 
+import io.github.charlietap.chasm.executor.runtime.encoder.isNullableReference
 import io.github.charlietap.chasm.executor.runtime.error.InvocationError
 import io.github.charlietap.chasm.executor.runtime.exception.InvocationException
 import io.github.charlietap.chasm.executor.runtime.execution.ExecutionContext
-import io.github.charlietap.chasm.executor.runtime.ext.peekReference
 import io.github.charlietap.chasm.executor.runtime.instruction.ReferenceInstruction
-import io.github.charlietap.chasm.executor.runtime.value.ReferenceValue
 
 internal inline fun RefAsNonNullExecutor(
     context: ExecutionContext,
     instruction: ReferenceInstruction.RefAsNonNull,
 ) {
     val stack = context.vstack
-    val value = stack.peekReference()
+    val value = stack.peek()
 
-    if (value is ReferenceValue.Null) {
+    if (value.isNullableReference()) {
         throw InvocationException(InvocationError.NonNullReferenceExpected)
     }
 }
--- executor/runtime-internal/src/commonMain/kotlin/io/github/charlietap/chasm/executor/runtime/encoder/ReferenceValueExt.kt
+++ executor/runtime-internal/src/commonMain/kotlin/io/github/charlietap/chasm/executor/runtime/encoder/ReferenceValueExt.kt
@@ -5,3 +5,5 @@ import io.github.charlietap.chasm.executor.runtime.value.ReferenceValue
 fun ReferenceValue.toLong(): Long = ReferenceValueEncoder(this)
 
 fun Long.toReferenceValue(): ReferenceValue = ReferenceValueDecoder(this)
+
+inline fun Long.isNullableReference(): Boolean = (this and RV_TYPE_MASK) == RV_TYPE_NULL
