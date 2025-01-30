8cfb994c4268a1fa42d2f451e571d85ab561b83f
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-01-30 19:14:20 +0000

    remove type substitution at runtime

--- executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/control/BrOnCastExecutor.kt
+++ executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/control/BrOnCastExecutor.kt
@@ -1,19 +1,13 @@
 package io.github.charlietap.chasm.executor.invoker.instruction.control
 
-import com.github.michaelbull.result.Result
 import io.github.charlietap.chasm.ast.module.Index
 import io.github.charlietap.chasm.ast.type.ReferenceType
 import io.github.charlietap.chasm.executor.invoker.type.TypeOf
 import io.github.charlietap.chasm.executor.invoker.type.TypeOfReferenceValue
-import io.github.charlietap.chasm.executor.runtime.error.InvocationError
 import io.github.charlietap.chasm.executor.runtime.execution.ExecutionContext
 import io.github.charlietap.chasm.executor.runtime.instruction.ControlInstruction
 import io.github.charlietap.chasm.type.matching.ReferenceTypeMatcher
 import io.github.charlietap.chasm.type.matching.TypeMatcher
-import io.github.charlietap.chasm.type.rolling.substitution.ReferenceTypeSubstitutor
-import io.github.charlietap.chasm.type.rolling.substitution.TypeSubstitutor
-
-internal typealias BrOnCastExecutor = (ExecutionContext, Index.LabelIndex, ReferenceType, ReferenceType, Boolean) -> Result<Unit, InvocationError>
 
 internal fun BrOnCastExecutor(
     context: ExecutionContext,
@@ -24,7 +18,6 @@ internal fun BrOnCastExecutor(
     referenceType1 = instruction.srcReferenceType,
     referenceType2 = instruction.dstReferenceType,
     breakIfMatches = true,
-    referenceTypeSubstitutor = ::ReferenceTypeSubstitutor,
     referenceTypeMatcher = ::ReferenceTypeMatcher,
     typeOfReferenceValue = ::TypeOfReferenceValue,
     breakExecutor = ::BreakExecutor,
@@ -40,7 +33,6 @@ internal fun BrOnCastFailExecutor(
         referenceType1 = instruction.srcReferenceType,
         referenceType2 = instruction.dstReferenceType,
         breakIfMatches = false,
-        referenceTypeSubstitutor = ::ReferenceTypeSubstitutor,
         referenceTypeMatcher = ::ReferenceTypeMatcher,
         typeOfReferenceValue = ::TypeOfReferenceValue,
         breakExecutor = ::BreakExecutor,
@@ -53,7 +45,6 @@ internal inline fun BrOnCastExecutor(
     referenceType1: ReferenceType,
     referenceType2: ReferenceType,
     breakIfMatches: Boolean,
-    crossinline referenceTypeSubstitutor: TypeSubstitutor<ReferenceType>,
     crossinline referenceTypeMatcher: TypeMatcher<ReferenceType>,
     crossinline typeOfReferenceValue: TypeOf<Long, ReferenceType>,
     crossinline breakExecutor: BreakExecutor,
@@ -63,10 +54,8 @@ internal inline fun BrOnCastExecutor(
     val frame = context.cstack.peekFrame()
     val moduleInstance = frame.instance
 
-    val closedReferenceType1 = typeOfReferenceValue(stack.peek(), store, moduleInstance)
-    val closedReferenceType2 = referenceTypeSubstitutor(referenceType2, context.substitutor)
-
-    val referenceTypeMatches = referenceTypeMatcher(closedReferenceType1, closedReferenceType2, context)
+    val referenceType = typeOfReferenceValue(stack.peek(), store, moduleInstance)
+    val referenceTypeMatches = referenceTypeMatcher(referenceType, referenceType2, context)
 
     if (referenceTypeMatches == breakIfMatches) {
         breakExecutor(context.cstack, stack, labelIndex)
