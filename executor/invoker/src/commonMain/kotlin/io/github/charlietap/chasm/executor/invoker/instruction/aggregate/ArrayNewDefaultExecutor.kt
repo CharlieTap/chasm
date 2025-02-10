331c68f7b534969619b6a83525381ef592d16fce
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-02-10 21:03:15 +0000

    inline array new fixed into array new default

--- executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/aggregate/ArrayNewDefaultExecutor.kt
+++ executor/invoker/src/commonMain/kotlin/io/github/charlietap/chasm/executor/invoker/instruction/aggregate/ArrayNewDefaultExecutor.kt
@@ -1,37 +1,27 @@
 package io.github.charlietap.chasm.executor.invoker.instruction.aggregate
 
 import io.github.charlietap.chasm.executor.runtime.execution.ExecutionContext
-import io.github.charlietap.chasm.executor.runtime.execution.Executor
+import io.github.charlietap.chasm.executor.runtime.ext.toLong
+import io.github.charlietap.chasm.executor.runtime.instance.ArrayInstance
 import io.github.charlietap.chasm.executor.runtime.instruction.AggregateInstruction
-
-internal fun ArrayNewDefaultExecutor(
-    context: ExecutionContext,
-    instruction: AggregateInstruction.ArrayNewDefault,
-) =
-    ArrayNewDefaultExecutor(
-        context = context,
-        instruction = instruction,
-        arrayNewFixedExecutor = ::ArrayNewFixedExecutor,
-    )
+import io.github.charlietap.chasm.executor.runtime.store.Address
+import io.github.charlietap.chasm.executor.runtime.value.ReferenceValue
 
 internal inline fun ArrayNewDefaultExecutor(
     context: ExecutionContext,
     instruction: AggregateInstruction.ArrayNewDefault,
-    crossinline arrayNewFixedExecutor: Executor<AggregateInstruction.ArrayNewFixed>,
 ) {
     val stack = context.vstack
+    val store = context.store
     val size = stack.popI32()
 
-    repeat(size) {
-        stack.push(instruction.field)
+    val fields = LongArray(size) {
+        instruction.field
     }
 
-    arrayNewFixedExecutor(
-        context,
-        AggregateInstruction.ArrayNewFixed(
-            definedType = instruction.definedType,
-            arrayType = instruction.arrayType,
-            size = size.toUInt(),
-        ),
-    )
+    val instance = ArrayInstance(instruction.definedType, instruction.arrayType, fields)
+    store.arrays.add(instance)
+    val reference = ReferenceValue.Array(Address.Array(store.arrays.size - 1))
+
+    stack.push(reference.toLong())
 }
