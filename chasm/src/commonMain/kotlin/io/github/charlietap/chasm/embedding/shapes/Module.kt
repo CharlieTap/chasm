1d025a45a70676b341d2a9bcf070cabda7a8bdc1
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-02-24 19:27:48 +0000

    reinstate module info

--- chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/shapes/Module.kt
+++ chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/shapes/Module.kt
@@ -1,21 +1,22 @@
 package io.github.charlietap.chasm.embedding.shapes
 
 import io.github.charlietap.chasm.config.ModuleConfig
+import io.github.charlietap.chasm.embedding.moduleInfo
 import io.github.charlietap.chasm.ast.module.Module as InternalModule
 
 class Module internal constructor(
     internal val config: ModuleConfig,
     internal val module: InternalModule,
 ) {
-//    private val info by lazy {
-//        moduleInfo(this)
-//    }
-//
-//    val imports by lazy {
-//        info.imports
-//    }
-//
-//    val exports by lazy {
-//        info.exports
-//    }
+    private val info by lazy {
+        moduleInfo(this)
+    }
+
+    val imports by lazy {
+        info.imports
+    }
+
+    val exports by lazy {
+        info.exports
+    }
 }
---
+++ chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/transform/ExportDescriptorMapper.kt
@@ -0,0 +1,47 @@
+package io.github.charlietap.chasm.embedding.transform
+
+import io.github.charlietap.chasm.ast.module.Export
+import io.github.charlietap.chasm.ast.module.Module
+import io.github.charlietap.chasm.executor.runtime.type.ExternalType
+import io.github.charlietap.chasm.type.ext.functionType
+
+internal class ExportDescriptorMapper(
+    private val module: Module,
+) : Mapper<Export.Descriptor, ExternalType> {
+
+    override fun map(input: Export.Descriptor): ExternalType {
+        return when (input) {
+            is Export.Descriptor.Function -> {
+                val function = module.functions.first { function ->
+                    function.idx == input.functionIndex
+                }
+                val functionType = module.types[function.typeIndex.idx.toInt()].recursiveType.functionType()
+                ExternalType.Function(functionType!!)
+            }
+            is Export.Descriptor.Global -> {
+                val global = module.globals.first { global ->
+                    global.idx == input.globalIndex
+                }
+                ExternalType.Global(global.type)
+            }
+            is Export.Descriptor.Memory -> {
+                val memory = module.memories.first { memory ->
+                    memory.idx == input.memoryIndex
+                }
+                ExternalType.Memory(memory.type)
+            }
+            is Export.Descriptor.Table -> {
+                val table = module.tables.first { table ->
+                    table.idx == input.tableIndex
+                }
+                ExternalType.Table(table.type)
+            }
+            is Export.Descriptor.Tag -> {
+                val tag = module.tags.first { tag ->
+                    tag.index == input.tagIndex
+                }
+                ExternalType.Tag(tag.type)
+            }
+        }
+    }
+}
