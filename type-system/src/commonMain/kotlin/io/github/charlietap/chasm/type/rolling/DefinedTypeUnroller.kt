f6593215bf03e6b0da4ba470a0dc7ac7afeea18b
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-03-20 19:46:13 +0000

    update doc surrounding defined type unrolling

--- type-system/src/commonMain/kotlin/io/github/charlietap/chasm/type/rolling/DefinedTypeUnroller.kt
+++ type-system/src/commonMain/kotlin/io/github/charlietap/chasm/type/rolling/DefinedTypeUnroller.kt
@@ -3,6 +3,14 @@ package io.github.charlietap.chasm.type.rolling
 import io.github.charlietap.chasm.type.DefinedType
 import io.github.charlietap.chasm.type.SubType
 
+/*
+    A DefinedType is a combination of a pointer to the root of a RecursiveType group
+    and its index into that group.
+    Unrolling effectively chases that pointer, yielding a specific RecursiveType, where
+    all of its internal and external references to other RecursiveTypes have been
+    replaced with ConcreteDefinedTypes.
+ */
+
 typealias DefinedTypeUnroller = (DefinedType) -> SubType
 
 fun DefinedTypeUnroller(
