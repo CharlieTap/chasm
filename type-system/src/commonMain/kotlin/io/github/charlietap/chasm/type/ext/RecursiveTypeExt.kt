3519448d2bc73a5a267783e778404053b8b88ec6
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-03-23 11:15:14 +0000

    remove usages to defined type rolling outside of factory creation

--- type-system/src/commonMain/kotlin/io/github/charlietap/chasm/type/ext/RecursiveTypeExt.kt
+++ type-system/src/commonMain/kotlin/io/github/charlietap/chasm/type/ext/RecursiveTypeExt.kt
@@ -6,23 +6,21 @@ import io.github.charlietap.chasm.type.FunctionType
 import io.github.charlietap.chasm.type.RecursiveType
 import io.github.charlietap.chasm.type.StructType
 import io.github.charlietap.chasm.type.expansion.DefinedTypeExpander
-import io.github.charlietap.chasm.type.rolling.DefinedTypeRoller
 
-inline fun RecursiveType.definedType(
-    definedTypeRoller: DefinedTypeRoller = ::DefinedTypeRoller,
-): DefinedType = definedTypeRoller(Int.MIN_VALUE, this).first()
+inline fun RecursiveType.definedType(): DefinedType = DefinedType(
+    typeIndex = Int.MIN_VALUE,
+    recursiveType = this,
+    recursiveTypeIndex = 0,
+)
 
 inline fun RecursiveType.functionType(
-    definedTypeRoller: DefinedTypeRoller = ::DefinedTypeRoller,
     definedTypeExpander: DefinedTypeExpander = ::DefinedTypeExpander,
-): FunctionType? = definedType(definedTypeRoller).functionType(definedTypeExpander)
+): FunctionType? = definedType().functionType(definedTypeExpander)
 
 inline fun RecursiveType.structType(
-    definedTypeRoller: DefinedTypeRoller = ::DefinedTypeRoller,
     definedTypeExpander: DefinedTypeExpander = ::DefinedTypeExpander,
-): StructType? = definedType(definedTypeRoller).structType(definedTypeExpander)
+): StructType? = definedType().structType(definedTypeExpander)
 
 inline fun RecursiveType.arrayType(
-    definedTypeRoller: DefinedTypeRoller = ::DefinedTypeRoller,
     definedTypeExpander: DefinedTypeExpander = ::DefinedTypeExpander,
-): ArrayType? = definedType(definedTypeRoller).arrayType(definedTypeExpander)
+): ArrayType? = definedType().arrayType(definedTypeExpander)
