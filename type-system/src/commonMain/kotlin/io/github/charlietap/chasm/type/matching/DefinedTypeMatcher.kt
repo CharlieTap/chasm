95780e4be4bdfe9cdedfc5b613fce01e3cc0f13c
CharlieTap <charlie.tapping@deliveroo.co.uk>
2025-03-23 20:33:45 +0000

    remove defined type substitution from defined type matching

--- type-system/src/commonMain/kotlin/io/github/charlietap/chasm/type/matching/DefinedTypeMatcher.kt
+++ type-system/src/commonMain/kotlin/io/github/charlietap/chasm/type/matching/DefinedTypeMatcher.kt
@@ -3,9 +3,6 @@ package io.github.charlietap.chasm.type.matching
 import io.github.charlietap.chasm.type.ConcreteHeapType
 import io.github.charlietap.chasm.type.DefinedType
 import io.github.charlietap.chasm.type.HeapType
-import io.github.charlietap.chasm.type.RecursiveType
-import io.github.charlietap.chasm.type.rolling.substitution.DefinedTypeSubstitutor
-import io.github.charlietap.chasm.type.rolling.substitution.TypeSubstitutor
 
 fun DefinedTypeMatcher(
     type1: DefinedType,
@@ -15,7 +12,6 @@ fun DefinedTypeMatcher(
     type1 = type1,
     type2 = type2,
     context = context,
-    definedTypeSubstitutor = ::DefinedTypeSubstitutor,
     heapTypeMatcher = ::HeapTypeMatcher,
 )
 
@@ -23,28 +19,14 @@ internal inline fun DefinedTypeMatcher(
     type1: DefinedType,
     type2: DefinedType,
     context: TypeMatcherContext,
-    crossinline definedTypeSubstitutor: TypeSubstitutor<DefinedType>,
     crossinline heapTypeMatcher: TypeMatcher<HeapType>,
 ): Boolean = when {
     type1 === type2 -> true
+    type1 == type2 -> true
     else -> {
-        val substitution = context.substitution
-
-        if (type1.recursiveType.state != RecursiveType.State.CLOSED) {
-            definedTypeSubstitutor(type1, substitution)
-        }
-
-        if (type2.recursiveType.state != RecursiveType.State.CLOSED) {
-            definedTypeSubstitutor(type2, substitution)
-        }
-
-        if (type1 == type2) {
-            true
-        } else {
-            val subType = context.unroller(type1)
-            subType.superTypes.any { ht1 ->
-                heapTypeMatcher(ht1, ConcreteHeapType.Defined(type2), context)
-            }
+        val subType = context.unroller(type1)
+        subType.superTypes.any { ht1 ->
+            heapTypeMatcher(ht1, ConcreteHeapType.Defined(type2), context)
         }
     }
 }
