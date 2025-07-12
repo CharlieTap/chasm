f69dc3cdf1d5634e3f3d01c98c88edb64f8b4165
CharlieTap <charlie.tapping@outlook.com>
2025-07-12 13:40:01 +0100

    add extension function for resolving name subsections

---
+++ chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/ext/ModuleExt.kt
@@ -0,0 +1,10 @@
+package io.github.charlietap.chasm.embedding.ext
+
+import io.github.charlietap.chasm.ast.module.Module
+import io.github.charlietap.chasm.ast.module.NameData
+import io.github.charlietap.chasm.ast.module.NameSubsection
+
+inline fun <reified T : NameSubsection> Module.nameSubsection(): T? {
+    val nameData = customs.filterIsInstance<NameData>().firstOrNull()
+    return nameData?.subsections?.filterIsInstance<T>()?.firstOrNull()
+}
