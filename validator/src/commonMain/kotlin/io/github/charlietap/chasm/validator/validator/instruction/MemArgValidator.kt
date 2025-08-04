2804f96fe5146229a5dcb436023db77e5aa37b50
CharlieTap <charlie.tapping@outlook.com>
2025-08-04 18:03:38 +0100

    validate memory offsets are in bounds when using 32 bit address space

--- validator/src/commonMain/kotlin/io/github/charlietap/chasm/validator/validator/instruction/MemArgValidator.kt
+++ validator/src/commonMain/kotlin/io/github/charlietap/chasm/validator/validator/instruction/MemArgValidator.kt
@@ -5,9 +5,13 @@ import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.binding
 import io.github.charlietap.chasm.ast.instruction.AtomicMemoryInstruction
 import io.github.charlietap.chasm.ast.instruction.MemArg
+import io.github.charlietap.chasm.ast.instruction.MemoryInstruction
+import io.github.charlietap.chasm.type.AddressType
+import io.github.charlietap.chasm.type.MemoryType
 import io.github.charlietap.chasm.validator.context.ValidationContext
 import io.github.charlietap.chasm.validator.error.InstructionValidatorError
 import io.github.charlietap.chasm.validator.error.ModuleValidatorError
+import io.github.charlietap.chasm.validator.ext.memoryType
 import io.github.charlietap.chasm.validator.ext.size
 import kotlin.math.pow
 
@@ -28,5 +32,20 @@ internal inline fun MemArgValidator(
         if (alignment > size) {
             Err(InstructionValidatorError.UnnaturalMemoryAlignment).bind<Unit>()
         }
+    }
+
+    val memoryType = context.instructionMemoryType().bind()
+    if (memoryType.addressType == AddressType.I32 && arg.offset > UInt.MAX_VALUE) {
+        Err(InstructionValidatorError.OutOfBounds).bind<Unit>()
+    }
+}
+
+private fun ValidationContext.instructionMemoryType(): Result<MemoryType, ModuleValidatorError> {
+    return when (val instruction = this.instruction) {
+        is MemoryInstruction.Load -> memoryType(instruction.memoryIndex)
+        is MemoryInstruction.Store -> memoryType(instruction.memoryIndex)
+        is AtomicMemoryInstruction.Load -> memoryType(instruction.memoryIndex)
+        is AtomicMemoryInstruction.Store -> memoryType(instruction.memoryIndex)
+        else -> Err(InstructionValidatorError.UnknownMemory)
     }
 }
