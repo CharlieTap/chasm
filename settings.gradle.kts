92fb46c5dcf774dbf118d806f635363978fbf054
CharlieTap <charlie.tapping@outlook.com>
2025-08-05 17:44:13 +0100

    update develocity to 4.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.develocity") version ("3.19")
+    id("com.gradle.develocity") version ("4.1")
     id("org.gradle.toolchains.foojay-resolver-convention") version("1.0.0")
 }
 
