89984ac3ad75e6d995a5a5fb8f3e45853d9bd601
CharlieTap <charlie.tapping@outlook.com>
2025-08-17 19:41:41 +0100

    make plugin logger internal

--- chasm-gradle-plugin/src/main/kotlin/io/github/charlietap/chasm/gradle/Logger.kt
+++ chasm-gradle-plugin/src/main/kotlin/io/github/charlietap/chasm/gradle/Logger.kt
@@ -2,13 +2,13 @@ package io.github.charlietap.chasm.gradle
 
 import org.gradle.api.logging.Logger as GradleLogger
 
-interface Logger {
+internal interface Logger {
     fun lifecycle(message: String)
 
     fun error(message: String)
 }
 
-class PluginLogger(
+internal class PluginLogger(
     private val logger: GradleLogger,
 ) : Logger {
     override fun lifecycle(message: String) {
