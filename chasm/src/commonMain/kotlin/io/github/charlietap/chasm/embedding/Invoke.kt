77221f9d727046e7df0c46d639948f42733f379c
CharlieTap <charlie.tapping@outlook.com>
2025-09-21 13:15:12 +0100

    add public api to invoke functions by reference

--- chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/Invoke.kt
+++ chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/Invoke.kt
@@ -6,9 +6,11 @@ import io.github.charlietap.chasm.embedding.error.ChasmError
 import io.github.charlietap.chasm.embedding.shapes.ChasmResult
 import io.github.charlietap.chasm.embedding.shapes.ChasmResult.Error
 import io.github.charlietap.chasm.embedding.shapes.ChasmResult.Success
+import io.github.charlietap.chasm.embedding.shapes.Function
 import io.github.charlietap.chasm.embedding.shapes.Instance
 import io.github.charlietap.chasm.embedding.shapes.Store
 import io.github.charlietap.chasm.executor.invoker.FunctionInvoker
+import io.github.charlietap.chasm.runtime.address.Address
 import io.github.charlietap.chasm.runtime.error.InvocationError
 import io.github.charlietap.chasm.runtime.error.ModuleTrapError
 import io.github.charlietap.chasm.runtime.instance.ExternalValue
@@ -17,27 +19,22 @@ import io.github.charlietap.chasm.runtime.value.ExecutionValue
 fun invoke(
     store: Store,
     instance: Instance,
-    name: String,
+    function: Function,
     args: List<ExecutionValue> = emptyList(),
 ): ChasmResult<List<ExecutionValue>, ChasmError.ExecutionError> = invoke(
     store = store,
     instance = instance,
-    name = name,
+    function = function,
     args = args,
     invoker = ::FunctionInvoker,
 )
 
-internal fun invoke(
+fun invoke(
     store: Store,
     instance: Instance,
     name: String,
-    args: List<ExecutionValue>,
-    invoker: FunctionInvoker,
+    args: List<ExecutionValue> = emptyList(),
 ): ChasmResult<List<ExecutionValue>, ChasmError.ExecutionError> {
-
-    if (instance.instance.deallocated) {
-        return Error(ChasmError.ExecutionError(InvocationError.InvocationOfADeinstantiatedInstance.toString()))
-    }
 
     val extern = instance.instance.exports
         .firstOrNull { export ->
@@ -47,6 +44,76 @@ internal fun invoke(
     val address = (extern as? ExternalValue.Function)?.address ?: return Error(
         ChasmError.ExecutionError(InvocationError.FunctionNotFound(name).toString()),
     )
+
+    return invoke(
+        store = store,
+        instance = instance,
+        address = address,
+        args = args,
+        invoker = ::FunctionInvoker,
+    )
+}
+
+internal fun invoke(
+    store: Store,
+    instance: Instance,
+    function: Function,
+    args: List<ExecutionValue> = emptyList(),
+    invoker: FunctionInvoker,
+): ChasmResult<List<ExecutionValue>, ChasmError.ExecutionError> = invoke(
+    store = store,
+    instance = instance,
+    address = function.reference.address,
+    args = args,
+    invoker = invoker,
+)
+
+internal fun invoke(
+    store: Store,
+    instance: Instance,
+    name: String,
+    args: List<ExecutionValue> = emptyList(),
+    invoker: FunctionInvoker,
+): ChasmResult<List<ExecutionValue>, ChasmError.ExecutionError> {
+
+    val extern = instance.instance.exports.firstOrNull { export ->
+        export.name.name == name
+    }?.value
+
+    val address = (extern as? ExternalValue.Function)?.address ?: return Error(
+        ChasmError.ExecutionError(InvocationError.FunctionNotFound(name).toString()),
+    )
+
+    return invoke(
+        store = store,
+        instance = instance,
+        address = address,
+        args = args,
+        invoker = invoker,
+    )
+}
+
+internal inline fun invoke(
+    store: Store,
+    instance: Instance,
+    address: Address.Function,
+    args: List<ExecutionValue>,
+    invoker: FunctionInvoker,
+): ChasmResult<List<ExecutionValue>, ChasmError.ExecutionError> {
+
+    if (instance.instance.deallocated) {
+        return Error(ChasmError.ExecutionError(InvocationError.InvocationOfADeinstantiatedInstance.toString()))
+    }
+
+    val validAddresses = instance.instance.exports.mapNotNull { export ->
+        (export.value as? ExternalValue.Function)?.address
+    }
+
+    if (!validAddresses.contains(address)) {
+        return Error(
+            ChasmError.ExecutionError(InvocationError.FunctionNotFound("Function with address: ${address.address}").toString()),
+        )
+    }
 
     return invoker(instance.config, store.store, address, args)
         .mapError(ModuleTrapError::toString)
--- chasm/src/commonTest/kotlin/io/github/charlietap/chasm/embedding/InvokeTest.kt
+++ chasm/src/commonTest/kotlin/io/github/charlietap/chasm/embedding/InvokeTest.kt
@@ -3,6 +3,7 @@ package io.github.charlietap.chasm.embedding
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import io.github.charlietap.chasm.embedding.error.ChasmError
+import io.github.charlietap.chasm.embedding.fixture.publicFunction
 import io.github.charlietap.chasm.embedding.fixture.publicInstance
 import io.github.charlietap.chasm.embedding.fixture.publicStore
 import io.github.charlietap.chasm.embedding.shapes.ChasmResult
@@ -22,24 +23,30 @@ import kotlin.test.assertEquals
 class InvokeTest {
 
     @Test
-    fun `calling invoke on an a deallocated instance returns InvocationOfADeinstantiatedInstance`() {
+    fun `calling invoke with a function by name on an a deallocated instance returns InvocationOfADeinstantiatedInstance`() {
 
         val store = publicStore()
         val instance = publicInstance(
             moduleInstance = moduleInstance(
                 deallocated = true,
+                exports = mutableListOf(
+                    exportInstance(
+                        nameValue("deallocated"),
+                        functionExternalValue(),
+                    ),
+                ),
             ),
         )
 
         val expected = ChasmError.ExecutionError(InvocationError.InvocationOfADeinstantiatedInstance.toString())
 
-        val actual = invoke(store, instance, "")
+        val actual = invoke(store, instance, "deallocated")
 
         assertEquals(ChasmResult.Error(expected), actual)
     }
 
     @Test
-    fun `calling invoke on an instance with a function that is not exported returns FunctionNotFound`() {
+    fun `calling invoke on an instance with a function by name is not exported returns FunctionNotFound`() {
 
         val function = "missing"
 
@@ -54,24 +61,179 @@ class InvokeTest {
     }
 
     @Test
-    fun `calling invoke on a function that fails propagates the invocation error`() {
+    fun `calling invoke with a function reference on a deallocated instance returns InvocationOfADeinstantiatedInstance`() {
 
-        val function = "fail"
+        val store = publicStore()
+        val address = functionAddress(117)
+        val functionExtern = functionExternalValue(address)
+        val function = publicFunction(functionExtern)
+        val instance = publicInstance(
+            moduleInstance = moduleInstance(
+                deallocated = true,
+                exports = mutableListOf(
+                    exportInstance(
+                        nameValue("deallocated"),
+                        functionExtern,
+                    ),
+                ),
+            ),
+        )
+
+        val expected = ChasmError.ExecutionError(InvocationError.InvocationOfADeinstantiatedInstance.toString())
+
+        val actual = invoke(store, instance, function)
+
+        assertEquals(ChasmResult.Error(expected), actual)
+    }
+
+    @Test
+    fun `calling invoke with a function reference that is not exported returns FunctionNotFound`() {
+
+        val store = publicStore()
+        val instance = publicInstance()
+        val address = functionAddress(117)
+        val function = publicFunction(
+            functionExternalValue(address),
+        )
+
+        val expected = ChasmError.ExecutionError(InvocationError.FunctionNotFound("Function with address: ${address.address}").toString())
+
+        val actual = invoke(store, instance, function)
+
+        assertEquals(ChasmResult.Error(expected), actual)
+    }
+
+    @Test
+    fun `calling invoke with a function by name that succeeds propagates the results`() {
 
         val store = publicStore()
         val args = emptyList<ExecutionValue>()
+        val name = "foo"
         val address = functionAddress(0)
-        val externalValue = functionExternalValue(address)
-        val exportInstance = exportInstance(
-            name = nameValue(function),
-            value = externalValue,
-        )
+
         val config = runtimeConfig()
         val instance = publicInstance(
             config = config,
             moduleInstance = moduleInstance(
                 exports = mutableListOf(
-                    exportInstance,
+                    exportInstance(
+                        nameValue(name),
+                        functionExternalValue(address),
+                    ),
+                ),
+            ),
+        )
+
+        val results = listOf(i32(117))
+        val functionInvoker: FunctionInvoker = { _config, _store, _address, _args ->
+            assertEquals(config, _config)
+            assertEquals(store.store, _store)
+            assertEquals(address, _address)
+            assertEquals(emptyList(), _args)
+
+            Ok(results)
+        }
+
+        val expected = ChasmResult.Success(listOf(i32(117)))
+
+        val actual = invoke(store, instance, name, args, functionInvoker)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `calling invoke with a function by name that fails propagates the invocation error`() {
+
+        val name = "foo"
+        val store = publicStore()
+        val args = emptyList<ExecutionValue>()
+        val address = functionAddress(0)
+        val config = runtimeConfig()
+        val instance = publicInstance(
+            config = config,
+            moduleInstance = moduleInstance(
+                exports = mutableListOf(
+                    exportInstance(
+                        nameValue(name),
+                        functionExternalValue(address),
+                    ),
+                ),
+            ),
+        )
+
+        val error = InvocationError.FunctionNotFound("function")
+        val functionInvoker: FunctionInvoker = { _config, _store, _address, _args ->
+            assertEquals(config, _config)
+            assertEquals(store.store, _store)
+            assertEquals(address, _address)
+            assertEquals(emptyList(), _args)
+
+            Err(error)
+        }
+
+        val expected = ChasmError.ExecutionError(error.toString())
+
+        val actual = invoke(store, instance, name, args, functionInvoker)
+
+        assertEquals(ChasmResult.Error(expected), actual)
+    }
+
+    @Test
+    fun `calling invoke with a function by reference that succeeds propagates the results`() {
+
+        val store = publicStore()
+        val args = emptyList<ExecutionValue>()
+        val address = functionAddress(0)
+        val functionExtern = functionExternalValue(address)
+        val function = publicFunction(functionExtern)
+
+        val config = runtimeConfig()
+        val instance = publicInstance(
+            config = config,
+            moduleInstance = moduleInstance(
+                exports = mutableListOf(
+                    exportInstance(
+                        nameValue(),
+                        functionExtern,
+                    ),
+                ),
+            ),
+        )
+
+        val results = listOf(i32(117))
+        val functionInvoker: FunctionInvoker = { _config, _store, _address, _args ->
+            assertEquals(config, _config)
+            assertEquals(store.store, _store)
+            assertEquals(address, _address)
+            assertEquals(emptyList(), _args)
+
+            Ok(results)
+        }
+
+        val expected = ChasmResult.Success(listOf(i32(117)))
+
+        val actual = invoke(store, instance, function, args, functionInvoker)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `calling invoke with a function by reference that fails propagates the invocation error`() {
+
+        val store = publicStore()
+        val args = emptyList<ExecutionValue>()
+        val address = functionAddress(0)
+        val functionExtern = functionExternalValue(address)
+        val function = publicFunction(functionExtern)
+        val config = runtimeConfig()
+        val instance = publicInstance(
+            config = config,
+            moduleInstance = moduleInstance(
+                exports = mutableListOf(
+                    exportInstance(
+                        nameValue(),
+                        functionExtern,
+                    ),
                 ),
             ),
         )
@@ -94,24 +256,21 @@ class InvokeTest {
     }
 
     @Test
-    fun `calling invoke on a function that succeeds propagates the results`() {
-
-        val function = "succeed"
+    fun `calling invoke with a function address that succeeds propagates the results`() {
 
         val store = publicStore()
         val args = emptyList<ExecutionValue>()
         val address = functionAddress(0)
-        val externalValue = functionExternalValue(address)
-        val exportInstance = exportInstance(
-            name = nameValue(function),
-            value = externalValue,
-        )
+
         val config = runtimeConfig()
         val instance = publicInstance(
             config = config,
             moduleInstance = moduleInstance(
                 exports = mutableListOf(
-                    exportInstance,
+                    exportInstance(
+                        nameValue(),
+                        functionExternalValue(address),
+                    ),
                 ),
             ),
         )
@@ -128,8 +287,44 @@ class InvokeTest {
 
         val expected = ChasmResult.Success(listOf(i32(117)))
 
-        val actual = invoke(store, instance, function, args, functionInvoker)
+        val actual = invoke(store, instance, address, args, functionInvoker)
 
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `calling invoke on a function address that fails propagates the invocation error`() {
+
+        val store = publicStore()
+        val args = emptyList<ExecutionValue>()
+        val address = functionAddress(0)
+        val config = runtimeConfig()
+        val instance = publicInstance(
+            config = config,
+            moduleInstance = moduleInstance(
+                exports = mutableListOf(
+                    exportInstance(
+                        nameValue(),
+                        functionExternalValue(address),
+                    ),
+                ),
+            ),
+        )
+
+        val error = InvocationError.FunctionNotFound("function")
+        val functionInvoker: FunctionInvoker = { _config, _store, _address, _args ->
+            assertEquals(config, _config)
+            assertEquals(store.store, _store)
+            assertEquals(address, _address)
+            assertEquals(emptyList(), _args)
+
+            Err(error)
+        }
+
+        val expected = ChasmError.ExecutionError(error.toString())
+
+        val actual = invoke(store, instance, address, args, functionInvoker)
+
+        assertEquals(ChasmResult.Error(expected), actual)
     }
 }
