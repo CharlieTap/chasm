9a638385df1c5f6f9680ee0f5cb6779f2af42bb8
CharlieTap <charlie.tapping@outlook.com>
2026-06-05 18:01:32 +0100

    add struct and array host context function bindings

--- chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/ext/HostFunctionContextExt.kt
+++ chasm/src/commonMain/kotlin/io/github/charlietap/chasm/embedding/ext/HostFunctionContextExt.kt
@@ -6,10 +6,16 @@ import io.github.charlietap.chasm.embedding.extern.readExternValue
 import io.github.charlietap.chasm.embedding.extern.readExternValueAs
 import io.github.charlietap.chasm.embedding.memory.readByte
 import io.github.charlietap.chasm.embedding.memory.readBytes
+import io.github.charlietap.chasm.embedding.reference.readArrayElement
+import io.github.charlietap.chasm.embedding.reference.readArrayLength
+import io.github.charlietap.chasm.embedding.reference.readStructField
+import io.github.charlietap.chasm.embedding.reference.writeArrayElement
+import io.github.charlietap.chasm.embedding.reference.writeStructField
 import io.github.charlietap.chasm.embedding.shapes.ChasmResult
 import io.github.charlietap.chasm.embedding.shapes.HostFunctionContext
 import io.github.charlietap.chasm.embedding.shapes.Memory
 import io.github.charlietap.chasm.embedding.shapes.map
+import io.github.charlietap.chasm.runtime.value.FieldValue
 import io.github.charlietap.chasm.runtime.value.ReferenceValue
 
 inline fun HostFunctionContext.byte(
@@ -95,3 +101,29 @@ fun HostFunctionContext.readExternValue(
 inline fun <reified T> HostFunctionContext.readExternValueAs(
     reference: ReferenceValue,
 ): ChasmResult<T?, ChasmError.ExecutionError> = readExternValueAs<T>(this.store, reference)
+
+fun HostFunctionContext.readStructField(
+    struct: ReferenceValue.Struct,
+    index: Int,
+): ChasmResult<FieldValue, ChasmError.ExecutionError> = readStructField(this.store, struct, index)
+
+fun HostFunctionContext.writeStructField(
+    struct: ReferenceValue.Struct,
+    index: Int,
+    value: FieldValue,
+): ChasmResult<Unit, ChasmError.ExecutionError> = writeStructField(this.store, struct, index, value)
+
+fun HostFunctionContext.readArrayLength(
+    array: ReferenceValue.Array,
+): ChasmResult<Int, ChasmError.ExecutionError> = readArrayLength(this.store, array)
+
+fun HostFunctionContext.readArrayElement(
+    array: ReferenceValue.Array,
+    index: Int,
+): ChasmResult<FieldValue, ChasmError.ExecutionError> = readArrayElement(this.store, array, index)
+
+fun HostFunctionContext.writeArrayElement(
+    array: ReferenceValue.Array,
+    index: Int,
+    value: FieldValue,
+): ChasmResult<Unit, ChasmError.ExecutionError> = writeArrayElement(this.store, array, index, value)
